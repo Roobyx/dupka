1f311e6af07145cef3394aec2ac2b4c87511f1ac
Marian Radev <radevhost@gmail.com>
2023-07-05 19:27:40 +0300

    Additional build fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@
 module.exports = {
 	root: true,
 	extends: '@react-native-community',
+	parserOptions: {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
 	indent: [ 4, 'tab' ],
 	semi: ["error", "never"],
 }
